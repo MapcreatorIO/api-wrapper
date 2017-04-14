7bae94cf6a0f4b4478f38cc1d72554e358aee558
shodan <mega@ioexception.at>
2017-04-14 12:27:01 +0200

    Add missing files

---
+++ src/util/reflection.js
@@ -0,0 +1,30 @@
+/**
+ * Tests if the parent is a parent of child
+ * @param {function|object} parent - Instance or Class
+ * @param {function|object} child - Instance or Class
+ * @returns {boolean}
+ *
+ * @example
+ * class A {}
+ * class B extends A {}
+ * class C extends B {}
+ *
+ * isParentOf(A, C) // true
+ * isParentOf(B, C) // true
+ * isParentOf(C, C) // true
+ * isParentOf(B, A) // false
+ */
+export function isParentOf(parent, child) {
+  parent = typeof parent === 'function' ? parent : parent.constructor;
+  child = typeof child === 'function' ? child : child.constructor;
+
+  do {
+    if (child.name === parent.name) {
+      return true;
+    }
+
+    child = Object.getPrototypeOf(child);
+  } while (child.name !== '');
+
+  return false;
+}
