5e3557ef7958fc88b5f4610afe8d32e2a5317004
shodan <mega@ioexception.at>
2017-05-03 12:29:00 +0200

    Split config into node(amd) and web (export to window)

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
+const webpackMerge = require('webpack-merge');
 
-
-module.exports = {
+const defaultConfig = {
+  target: 'node',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/node'),
 
     library: 'maps4news',
     libraryTarget: 'amd',
@@ -31,3 +32,14 @@ module.exports = {
     new webpack.BannerPlugin(fs.readFileSync('LICENSE', 'ascii')),
   ],
 };
+
+module.exports = [
+  defaultConfig,
+  webpackMerge(defaultConfig, {
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, 'dist/web'),
+      libraryTarget: 'window',
+    },
+  }),
+];
