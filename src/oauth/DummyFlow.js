b68a8fc53311416eb7d8ea8dc8aeb1bab88b0b04
shodan <mega@ioexception.at>
2017-05-04 17:40:48 +0200

    Make DummyFlow resolve if the a token was found

--- src/oauth/DummyFlow.js
+++ src/oauth/DummyFlow.js
@@ -1,15 +1,23 @@
 import OAuthError from './OAuthError';
 import OAuth from './OAuth';
 
+/**
+ * Dummy flow for when the token should be available in
+ * the cache and no attempt at authentication should be made.
+ */
 export default class DummyFlow extends OAuth {
 
   /**
    * Authenticate
-   * @returns {Promise} - Always rejects with OAuthError
+   * @returns {Promise} - Promise resolves with OAuthToken and rejects with OAuthError
    */
   authenticate() {
     return new Promise((resolve, reject) => {
-      reject(new OAuthError('dummy_error', 'Attempted authentication using a dummy authenticator'));
+      if (this.authenticated) {
+        resolve(this.token);
+      } else {
+        reject(new OAuthError('dummy_error', 'Attempted authentication using a dummy authenticator'));
+      }
     });
   }
 }
