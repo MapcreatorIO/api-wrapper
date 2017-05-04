6436296d4bd2b3cd73368e0d10b7a32eba22526d
shodan <mega@ioexception.at>
2017-05-04 18:42:13 +0200

    Add docs for Maps4News.js

--- src/Maps4News.js
+++ src/Maps4News.js
@@ -35,6 +35,9 @@ import Svg from './crud/Svg';
 import SvgSetType from './crud/SvgSetType';
 import SvgSet from './crud/SvgSet';
 
+/**
+ * Base API class
+ */
 export default class Maps4News {
   constructor(auth, host = 'https://api.maps4news.com') {
     if (!isParentOf(OAuth, auth)) {
@@ -50,15 +53,27 @@ export default class Maps4News {
     return this.auth.authenticated;
   }
 
+  /**
+   * The current host
+   * @returns {string} - the current host
+   */
   get host() {
     return this._host;
   }
 
+  /**
+   * The remote host
+   * @param {string} value - A valid url
+   */
   set host(value) {
     this._host = value;
     this.auth.host = value;
   }
 
+  /**
+   * Authenticate with the api using the authentication method provided.
+   * @returns {Promise} - resolves with {@link Maps4News} instance and rejects with {@link OAuthError}
+   */
   authenticate() {
     return new Promise((resolve, reject) => {
       this.auth.authenticate().then(() => {
@@ -69,22 +84,17 @@ export default class Maps4News {
     });
   }
 
-  logout() {
-    const prefix = 'm4n_api_';
-
-    for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      const keyPrefix = key.slice(0, prefix.length);
-
-      if (keyPrefix === prefix) {
-        localStorage.removeItem(key);
-      }
-    }
-
-    this.auth.token = null;
-  }
-
-  // Basic authenticated requests with error handling
+  /**
+   * Request an url using the API token (if available)
+   * @param {string} url - Relative or absolute url, api version will be prepended to relative urls
+   * @param {string} method - Http method
+   * @param {string|object} data - Raw string or object. If an object is passed it will be encoded
+   *                               and the content-type will be set to `application/json`
+   * @param {object} headers - Any headers that should be set for the request
+   * @param {string} responseType - The XmlHttpRequest type
+   * @param {boolean} raw - When set to true the promise will resolve with the request object
+   * @returns {Promise} - resolves with either an object or the raw data by checking the `Content-Type` header and rejects with {@link ApiError}
+   */
   request(url, method = 'GET', data = {}, headers = {}, responseType = '', raw = false) {
     if (!url.startsWith('http')) {
       // Removes '/' at the start of the string (if any)
@@ -92,7 +102,9 @@ export default class Maps4News {
       url = this._host + '/' + this.version + '/' + url;
     }
 
-    headers.Authorization = this.auth.token.toString();
+    if (this.authenticated) {
+      headers.Authorization = this.auth.token.toString();
+    }
 
     return new Promise((resolve, reject) => {
       makeRequest(url, method, data, headers, responseType).then(request => {
@@ -123,111 +135,245 @@ export default class Maps4News {
     });
   }
 
-  // resource bindings
+  /**
+   * Choropleth accessor
+   * @see {@link Choropleth}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get choropleths() {
     return new ResourceProxy(this, Choropleth);
   }
 
+  /**
+   * Color accessor
+   * @see {@link Color}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get colors() {
     return new ResourceProxy(this, Color);
   }
 
+  /**
+   * Contract accessor
+   * @see {@link Contract}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get contracts() {
     return new ResourceProxy(this, Contract);
   }
 
+  /**
+   * Dimension accessor
+   * @see {@link Dimension}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get dimensions() {
     return new ResourceProxy(this, Dimension);
   }
 
+  /**
+   * Dimension set accessor
+   * @see {@link DimensionSet}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get dimensionSets() {
     return new ResourceProxy(this, DimensionSet);
   }
 
+  /**
+   * Faq accessor
+   * @see {@link Faq}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get faqs() {
     return new ResourceProxy(this, Faq);
   }
 
+  /**
+   * Feature accessor
+   * @see {@link Feature}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get features() {
     return new ResourceProxy(this, Feature);
   }
 
+  /**
+   * Font accessor
+   * @see {@link Font}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get fonts() {
     return new ResourceProxy(this, Font);
   }
 
+  /**
+   * FontFamily accessor
+   * @see {@link FontFamily}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get fontFamilies() {
     return new ResourceProxy(this, FontFamily);
   }
 
+  /**
+   * Group accessor
+   * @see {@link Group}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get groups() {
     return new ResourceProxy(this, Group);
   }
 
+  /**
+   * Highlight accessor
+   * @see {@link Highlight}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get highlights() {
     return new ResourceProxy(this, Highlight);
   }
 
+  /**
+   * InsetMap accessor
+   * @see {@link InsetMap}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get insetMaps() {
     return new ResourceProxy(this, InsetMap);
   }
 
+  /**
+   * Job accessor
+   * @see {@link Job}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get jobs() {
     return new ResourceProxy(this, Job);
   }
 
+  /**
+   * JobShare accessor
+   * @see {@link JobShare}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get jobShares() {
     return new ResourceProxy(this, JobShare);
   }
 
+  /**
+   * JobType accessor
+   * @see {@link JobType}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get jobTypes() {
     return new ResourceProxy(this, JobType);
   }
 
+  /**
+   * Language accessor
+   * @see {@link Language}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get languages() {
     return new ResourceProxy(this, Language);
   }
 
+  /**
+   * Layer accessor
+   * @see {@link Layer}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get layers() {
     return new ResourceProxy(this, Layer);
   }
 
+  /**
+   * Mapstyle accessor
+   * @see {@link Mapstyle}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get mapstyles() {
     return new ResourceProxy(this, Mapstyle);
   }
 
+  /**
+   * MapstyleSet accessor
+   * @see {@link MapstyleSet}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get mapstylesSets() {
     return new ResourceProxy(this, MapstyleSet);
   }
 
+  /**
+   * Notification accessor
+   * @see {@link Notification}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get notifications() {
     return new ResourceProxy(this, Notification);
   }
 
+  /**
+   * Organisation accessor
+   * @see {@link Organisation}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get organisations() {
     return new ResourceProxy(this, Organisation);
   }
 
+  /**
+   * Permission accessor
+   * @see {@link Permission}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get permissions() {
     return new ResourceProxy(this, Permission);
   }
 
+  /**
+   * PlaceName accessor
+   * @see {@link PlaceName}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get placeNames() {
     return new ResourceProxy(this, PlaceName);
   }
 
+  /**
+   * Svg accessor
+   * @see {@link Svg}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get svgs() {
     return new ResourceProxy(this, Svg);
   }
 
+  /**
+   * SvgSet accessor
+   * @see {@link SvgSet}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get svgSets() {
     return new ResourceProxy(this, SvgSet);
   }
 
+  /**
+   * SvgSetType accessor
+   * @see {@link SvgSetType}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get svgSetTypes() {
     return new ResourceProxy(this, SvgSetType);
   }
 
+  /**
+   * User accessor
+   * @see {@link User}
+   * @returns {ResourceProxy} - A proxy for accessing the resource
+   */
   get users() {
     return new ResourceProxy(this, User);
   }
