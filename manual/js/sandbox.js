a78a7c93aab7874c957f6f24cbc4ee42bfcb1321
shodan <mega@ioexception.at>
2017-06-19 15:19:15 +0200

    Fix sandbox

--- manual/js/sandbox.js
+++ manual/js/sandbox.js
@@ -4,26 +4,23 @@
   }
 })();
 
-(function() {
+
+function easyAuth() {
+  var m4n = window.maps4news;
+
+  var clientId = 3;
   var docsUrl = 'https://mapcreatoreu.github.io/m4n-api/';
+  var auth = new m4n.ImplicitFlowPopup(clientId, docsUrl);
+  var api = new m4n.Maps4News(auth);
 
-  if(window.location.toString().startsWith(docsUrl)) {
-    function easyAuth() {
-      var m4n = window.maps4news;
+  api.host = 'https://api.beta.maps4news.com/';
 
-      var clientId = 3;
-      var auth = new m4n.ImplicitFlowPopup(clientId, docsUrl);
-      var api = new m4n.Maps4News(auth);
+  return api.authenticate();
+}
 
-      api.host = 'https://api.beta.maps4news.com/';
+console.log('You can play around with the api in here, the library is bound to window.maps4news. Authentication ');
+console.log('can easily be done by running the command easyAuth(). See the docs for more information here: ');
+console.log('https://mapcreatoreu.github.io/m4n-api/manual/example/examples.authentication.html');
+console.log('Example: easyAuth().then(api => window.api = api);');
+console.log('window.maps4news =', window.maps4news);
 
-      return api.authenticate();
-    }
-
-    console.log('You can play around with the api in here, the library is bound to window.maps4news. Authentication ');
-    console.log('can easily be done by running the command easyAuth(). See the docs for more information here: ');
-    console.log('https://mapcreatoreu.github.io/m4n-api/manual/example/examples.authentication.html');
-    console.log('\nwindow.maps4news =', window.maps4news);
-    console.log('Example: easyAuth().then(api => window.api = api);');
-  }
-})();
