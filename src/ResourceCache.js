70869d6bc9b2f9de423183c25d69d3687d522079
shodan <mega@ioexception.at>
2017-07-12 17:04:31 +0200

    docs etc

--- src/ResourceCache.js
+++ src/ResourceCache.js
@@ -1,5 +1,10 @@
 import mitt from 'mitt';
 
+
+/**
+ * Used for caching resources
+ * @see {@link PaginatedResourceWrapper}
+ */
 export default class ResourceCache {
   constructor(api, cacheTime = api.defaults.cacheSeconds) {
     this._api = api;
@@ -15,21 +20,33 @@ export default class ResourceCache {
     // push should send an event for rebuilding
   }
 
+  /**
+   * Push a page into the cache
+   * @param {PaginatedResourceListing} page - Data to be cached
+   */
   push(page) {
+    const validThrough = this._timestamp() + this.cacheTime;
     const data = {
-      page: page,
+      page, validThrough,
       timeout: setTimeout(
         () => this.revalidate(page.url),
         this.cacheTime * 1000,
       ),
-      validThrough: this._timestamp() + this.cacheTime,
     };
 
     const storage = this._storage[page.url] || (this._storage[page.url] = {});
 
     (storage[page.cacheToken] || (storage[page.cacheToken] = [])).push(data);
+
+    this.emitter.emit('push', {page, validThrough});
+    this.emitter.emit('invalidate', {resourceUrl: page.url});
   }
 
+  /**
+   * Revalidate all data and delete stale data
+   * @param {String} resourceUrl - Resource url
+   * @returns {void}
+   */
   revalidate(resourceUrl = null) {
     if (!resourceUrl) {
       Object.keys(this._storage).map(this.revalidate);
@@ -45,17 +62,27 @@ export default class ResourceCache {
         storage[key] = storage[key].filter(row => row.validThrough >= this._timestamp());
       });
 
-      // Delete empty
-      Object.keys(storage)
-        .filter(key => storage[key].length === 0)
-        .forEach(key => delete storage[key]);
+      const junk = Object.keys(storage).filter(key => storage[key].length === 0);
 
+      // Delete empty
+      junk.forEach(key => delete storage[key]);
       if (Object.keys(storage).length === 0) {
         delete this._storage[resourceUrl];
+      }
+
+      if (junk.length > 0) {
+        this.emitter.emit('invalidate', {resourceUrl});
       }
     }
   }
 
+  /**
+   * Collect relevant cached pages
+   * @param {String} resourceUrl - resource url
+   * @param {String} cacheToken - Cache token
+   * @see {@link PaginatedResourceListing#cacheToken}
+   * @returns {Array<PaginatedResourceListing>} - Relevant cached pages
+   */
   collectPages(resourceUrl, cacheToken = '') {
     const storage = this._storage[resourceUrl] || {};
 
@@ -68,18 +95,35 @@ export default class ResourceCache {
       .map(x => x.page);
   }
 
-  clear(url = '') {
-    if (!url) {
+  /**
+   * Clears the cache
+   * @param {String} resourceUrl - Resource url
+   * @returns {void}
+   */
+  clear(resourceUrl = '') {
+    if (!resourceUrl) {
+      Object.keys(this._storage).forEach(url => {
+        this.emitter.emit('invalidate', {resourceUrl: url});
+      });
+
       this._storage = {};
     } else {
-      this._storage[url] = {};
+      delete this._storage[resourceUrl];
+      this.emitter.emit('invalidate', {resourceUrl});
     }
   }
 
-  resolve(url, token = '') {
-    token = token.toLowerCase();
+  /**
+   * Resolve cache and return indexed data
+   * @param {String} resourceUrl - Resource url
+   * @param {String} cacheToken - Cache token
+   * @see {@link PaginatedResourceListing#cacheToken}
+   * @returns {Array} - Indexed relevant data
+   */
+  resolve(resourceUrl, cacheToken = '') {
+    cacheToken = cacheToken.toLowerCase();
 
-    const data = this.collectPages(url, token);
+    const data = this.collectPages(resourceUrl, cacheToken);
     const out = [];
 
     for (const page of data) {
@@ -104,6 +148,11 @@ export default class ResourceCache {
     return out;
   }
 
+  /**
+   * Get a usable timestamp
+   * @returns {number} - timestamp
+   * @private
+   */
   _timestamp() {
     return Math.floor(Date.now() / 1000);
   }
