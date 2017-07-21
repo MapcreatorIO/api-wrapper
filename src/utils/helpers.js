c8fa9b6469b7d8c79f14c6951750b10a6f869251
shodan <mega@ioexception.at>
2017-07-21 16:27:39 +0200

    getPaginatedRange docs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,30 +1,45 @@
-// it will have to wait for the first request :<
 /**
- *
- * @param {Promise} promise - Promise that returns a {@link PaginatedResourceWrapper}
+ * Get all the pages from a {@link PaginatedResourceListing} or a range
+ * @param {Promise<PaginatedResourceListing>|PaginatedResourceListing} page - Promise that returns a {@link PaginatedResourceWrapper}
  * @param {?Number} [start=1] - Start page
  * @param {?Number} [stop] - Stop page
- * @returns {Promise}
+ * @returns {Promise<Array<ResourceBase>>} - Resolves with an {@link Array} containing {@link PaginatedResourceListing} instance and rejects with {@link ApiError}
+ * @example
+ * const first = api.users.list();
+ *
+ * getPaginatedRange(promise).then(data => {
+ *    data.map(row => `[${row.id}] ${row.name}`) // We just want the names
+ *        .forEach(console.log) // Log the names and ids of every user
+ * })
  */
-export function getPaginatedRange(promise, start = 1, stop) {
-  return promise.then(page => {
-    const out = page.data;
-    const promises = [];
+export function getPaginatedRange(page, start = 1, stop) {
+  // Resolve promise if any
+  if (page instanceof Promise) {
+    return page.then(res => getPaginatedRange(res, start, stop));
+  }
 
-    // Handle defaults
-    start = start || page.page;
-    stop = stop || page.pageCount;
+  const out = page.data;
+  const promises = [];
 
-    return new Promise((resolve, reject) => {
-      for (let i = start + 1; i <= stop; i++) {
-        promises.push(page.get(i));
-      }
+  // Handle defaults
+  start = start || page.page;
+  stop = stop || page.pageCount;
 
-      Promise.all(promises).then(rows => {
-        rows.map(x => x.data).forEach(out.push);
+  if (start === page.page) {
+    start++;
+  }
 
-        resolve(out);
-      }, reject);
-    });
+  return new Promise((resolve, reject) => {
+    // Get all pages
+    for (let i = start; i <= stop; i++) {
+      promises.push(page.get(i));
+    }
+
+    // Resolve
+    Promise.all(promises).then(rows => {
+      rows.map(x => x.data).forEach(out.push);
+
+      resolve(out);
+    }, reject);
   });
 }
