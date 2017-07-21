6c25ac1f19ac5953339c0bb85645516d767d7cf9
shodan <mega@ioexception.at>
2017-07-21 14:33:34 +0200

    Remove crypto api uuid4 generator

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -11,19 +11,6 @@ export default class Uuid extends StaticClass {
    * @returns {string} - Uuid
    */
   static uuid4() {
-    // Use the secure method if possible
-    return typeof crypto !== 'undefined' && crypto.getRandomValues ? Uuid._uuid4Safe() : Uuid._uuid4Unsafe();
-  }
-
-  /**
-   * Unsafe UUID4 generation using Math.random
-   * @returns {string} - Uuid
-   *
-   * @see http://stackoverflow.com/a/8809472
-   * @license MIT|Public Domain
-   * @private
-   */
-  static _uuid4Unsafe() {
     let d = new Date().getTime();
 
     if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
@@ -35,26 +22,6 @@ export default class Uuid extends StaticClass {
 
       d = Math.floor(d / 16);
       return (c === 'x' ? r : r & 0x3 | 0x8).toString(16);
-    });
-  }
-
-  /**
-   * Safe UUID4 generation using the crypto api
-   * @returns {String} - Uuid
-   * @private
-   */
-  static _uuid4Safe() {
-    let data = new Uint8Array(16);
-
-    crypto.getRandomValues(data);
-
-    // cast to array so we can use pop()
-    data = [].slice.call(data);
-
-    // Replace 'xx' with a two width base 16 number
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/xx/g, () => {
-      // Prepend 00 for padding and grab the last two characters
-      return ('00' + data.pop().toString(16)).slice(-2);
     });
   }
 
