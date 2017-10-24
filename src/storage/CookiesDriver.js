dec8ea01c315fdb831c7d77db618732cf27318e1
Bas <mega@ioexception.at>
2017-10-24 15:47:11 +0200

    Add storage drivers

---
+++ src/storage/CookiesDriver.js
@@ -0,0 +1,121 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2017, MapCreator
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import DataStoreDriver from './DataStoreDriver';
+
+export default class CookiesDriver extends DataStoreDriver {
+  constructor(prefix = '_m4n_') {
+    super();
+
+    this.__prefix = prefix;
+  }
+
+  /**
+   * Cookie name prefix
+   * @returns {String} - Prefix
+   * @private
+   */
+  static get _prefix() {
+    return this.__prefix;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  static get secure() {
+    return window.location.protocol === 'https:';
+  }
+
+  /**
+   * Store a value in the storage
+   * @param {String} name - value name
+   * @param {*} value - value
+   * @param {Date|String} [expires=2050-01-01] - Expiration date
+   * @returns {void}
+   */
+  set(name, value, expires = new Date('2050-01-01')) {
+    name = encodeURIComponent(CookiesDriver._prefix + name);
+    value = encodeURIComponent(value);
+
+    if (expires instanceof Date) {
+      expires = expires.toUTCString();
+    }
+
+    document.cookie = `${name}=${value}; expires=${expires}`;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  get(name) {
+    name = CookiesDriver._prefix + name;
+
+    return this._toObject()[name];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  remove(name) {
+    name = encodeURIComponent(CookiesDriver._prefix + name);
+
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  keys() {
+    const regex = new RegExp('^' + CookiesDriver._prefix);
+
+    return Object.keys(this._toObject()).map(x => x.replace(regex, ''));
+  }
+
+  /**
+   * Extract cookies and turn them into a object
+   * @returns {Object} - cookies
+   * @private
+   */
+  _toObject() {
+    const cookies = {};
+
+    document.cookie
+      .split(';')
+      .map(x => x.trim().split('=').map(decodeURIComponent))
+      .filter(x => x[0].startsWith(CookiesDriver._prefix))
+      .forEach(x => {
+        cookies[x[0]] = x[1];
+      });
+
+    return cookies;
+  }
+}
---
+++ src/storage/LocalStorageDriver.js
@@ -0,0 +1,100 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2017, MapCreator
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import DataStoreDriver from './DataStoreDriver';
+
+export default class LocalStorageDriver extends DataStoreDriver {
+  /**
+   * LocalStorage name prefix
+   * @returns {String} - Prefix
+   * @private
+   */
+  static get _prefix() {
+    return '_m4n_';
+  }
+
+  /**
+   * Store a value in the storage
+   * @param {String} name - value name
+   * @param {*} value - value
+   * @returns {void}
+   */
+  set(name, value) {
+    name = LocalStorageDriver._prefix + name;
+
+    window.localStorage.setItem(name, value);
+  }
+
+  /**
+   * Get a value from the store
+   * @param {String} name - value name
+   * @returns {void}
+   */
+  get(name) {
+    name = LocalStorageDriver._prefix + name;
+
+    return window.localStorage.getItem(name);
+  }
+
+  /**
+   * Remove a value from the store
+   * @param {String} name - value name
+   * @returns {void}
+   */
+  remove(name) {
+    name = LocalStorageDriver._prefix + name;
+
+    window.localStorage.removeItem(name);
+  }
+
+  /**
+   * Storage keys
+   * @returns {Array<String>} - Stored keys
+   */
+  keys() {
+    const keys = [];
+    const storage = window.localStorage;
+    const prefix = LocalStorageDriver._prefix;
+
+    for (let i = 0; i < storage.length; i++) {
+      let key = storage.key(i);
+
+      if (key.startsWith(prefix)) {
+        key = key.replace(new RegExp(`^${prefix}`), '');
+
+        keys.push(key);
+      }
+    }
+
+    return keys;
+  }
+}
