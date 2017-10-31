b79e6be457a16ec0d75e0aa6fbd64211576626a3
Bas <mega@ioexception.at>
2017-10-31 16:05:46 +0100

    Remove tap-dot dependency

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -8,4 +8,5 @@ fi
 
 mkdir -pv build
 
-npx nyc --all --report-dir build/coverage --clean -n 'src/**.js' -r html -r cobertura npx ava -t | tee build/ava.tap | npx tap-dot
+npx nyc --all --report-dir build/coverage --clean -n 'src/**.js' -r html -r cobertura npx ava -t > build/ava.tap
+npx tap-nyc < build/ava.tap
