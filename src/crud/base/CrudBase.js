16dc50ef81b0d063c2abcc1f4380fabb2244b93c
Bas <mega@ioexception.at>
2017-11-10 15:55:21 +0100

    fix resolving empty promise

--- src/crud/base/CrudBase.js
+++ src/crud/base/CrudBase.js
@@ -117,7 +117,7 @@ export default class CrudBase extends ResourceBase {
     // We'll just fake it, no need to bother the server
     // with an empty request.
     if (Object.keys(this._properties).length === 0) {
-      return new Promise(() => this);
+      return new Promise(resolve => resolve(this));
     }
 
     return this.api
