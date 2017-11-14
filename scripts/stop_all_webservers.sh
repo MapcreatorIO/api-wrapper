027bb7ce79a6ba2ed1d5337bcda0e605dfaa0c05
Bas <mega@ioexception.at>
2017-11-14 12:11:42 +0100

    Echo stopped webserver port

--- scripts/stop_all_webservers.sh
+++ scripts/stop_all_webservers.sh
@@ -7,6 +7,8 @@ fi
 for pidFile in build/http-*.pid
 do
   if [ "$pidFile" != "build/http-*.pid" ]; then
+    port=`echo ${pidFile} | grep -o -- '-[0-9]*' | sed s/-//`
+    echo "Stopping webserver on port $port"
     kill `cat "$pidFile"` 2>&- >&-
     rm "$pidFile"
   fi
