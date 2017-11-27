25789cb766153edd1919d1c817b45b35dc30221d
Bas <mega@ioexception.at>
2017-11-27 11:25:27 +0100

    JobMonitorRow model

---
+++ src/JobMonitorRow.js
@@ -0,0 +1,83 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2017, MapCreator
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import ResourceBase from './crud/base/ResourceBase';
+
+export default class JobMonitorRow extends ResourceBase {
+
+  /**
+   * Guess type based on property name
+   * @param {string} name - Field name
+   * @param {*} value - Field Value
+   * @private
+   * @returns {*} - Original or converted value
+   */
+  static _guessType(name, value) {
+    if (['dealt_with', 'bought'].includes(name)) {
+      return Boolean(value);
+    }
+
+    return super._guessType(name, value);
+  }
+
+  /**
+   * Returns if the resource is readonly
+   * @returns {boolean} - readonly
+   */
+  static get readonly() {
+    return true;
+  }
+
+  /**
+   * Get the related job
+   * @returns {Promise<Job, ApiError>} - The job related to this row
+   */
+  get job() {
+    return this.api.jobs.get(this.jobId);
+  }
+
+  /**
+   * Get the related job revision
+   * @returns {Promise<JobRevision, ApiError>} - The job revision related to this row
+   */
+  get jobRevision() {
+    return this.api.jobs.select(this.jobId).revisions.get(this.id);
+  }
+
+  /**
+   * Get the related job
+   * @returns {Promise<Job, ApiError>} - The job related to this row
+   */
+  get user() {
+    return this.api.users.get(this.userId);
+  }
+}
