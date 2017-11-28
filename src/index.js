ed49823a1389fec7e012e4a721be28a8e3328fa4
Bas <mega@ioexception.at>
2017-11-28 12:22:28 +0100

    Fix enum exports

--- src/index.js
+++ src/index.js
@@ -38,9 +38,7 @@ export JobMonitor from './JobMonitor';
 
 // Enums
 export Enum from './enums/Enum';
-export DeletedState from './enums/DeletedState';
-export JobMonitorFilter from './enums/JobMonitorFilter';
-export ResultStatus from './enums/ResultStatus';
+export {DeletedState, JobMonitorFilter, ResultStatus} from './enums';
 
 // Flows
 export OAuth from './oauth/OAuth';
