9c5691ca1754b1437fd9fedcfdb2cc8fa118548f
Bas <mega@ioexception.at>
2018-05-08 14:18:34 +0200

    Fix preview fetching using JobMonitorRow

--- src/resources/JobMonitorRow.js
+++ src/resources/JobMonitorRow.js
@@ -33,10 +33,6 @@
 import JobResult from './JobResult';
 
 export default class JobMonitorRow extends JobResult {
-  get resourcePath() {
-    return '/jobs/{job_id}/revisions/{revision}';
-  }
-
   /**
    * Guess type based on property name
    * @param {string} name - Field name
