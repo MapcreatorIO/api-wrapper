0c30429006149b5cda972e9289ddba02b436865b
Bas <mega@ioexception.at>
2018-08-13 23:09:32 +0200

    Fix node support

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,6 +11,10 @@ mkdir -pv build
 # build
 npx webpack
 
+# Our dirty little secret
+awk 'NR==1,/window/{sub(/window/, "typeof window === \"undefined\" ? global : window")} 1'  dist/bundle.js > dist/bundle.fix.js
+mv dist/bundle.fix.js dist/bundle.js
+
 # Remove empty sourcemap and references
 rm dist/bundle.min.js.map
 sed -e '$ d' dist/bundle.min.js > dist/bundle.min.js~
