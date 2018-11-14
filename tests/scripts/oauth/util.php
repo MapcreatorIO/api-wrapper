767469c9c72107be9e91ac912c57b704c4c47377
Bas <mega@ioexception.at>
2018-11-14 10:59:56 +0100

    Update license

--- tests/scripts/oauth/util.php
+++ tests/scripts/oauth/util.php
@@ -2,7 +2,7 @@
 /**
  * BSD 3-Clause License
  *
- * Copyright (c) 2017, MapCreator
+ * Copyright (c) 2018, MapCreator
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
