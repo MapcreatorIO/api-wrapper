33e063de6ce025b2d4854d12a038fdbcaf298f29
Bas <Mechazawa@users.noreply.github.com>
2020-04-09 13:30:38 +0200

    Update copyright (#126)
    * Update copyright
    
    * MapCreator => Mapcreator
    
    * Fix test

--- src/oauth/DummyFlow.js
+++ src/oauth/DummyFlow.js
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2018, MapCreator
+ * Copyright (c) 2020, Mapcreator
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
